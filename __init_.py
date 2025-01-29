187fb6133ce4ace8c56cba4a80060263ef69edf1
Darryl Johnson <dandycodes@gmail.com>
2025-01-29 12:42:05 -0500

    Entered API formatting

--- __init_.py
+++ __init_.py
@@ -1,3 +1,21 @@
 # Install OpenAI SDK "pip3 install openai"
 
 from openai import openAI
+
+# Invoke client API
+
+client = OpenAI(api_key="< INSERT KEY HERE>", base_url="https://apei.deepseek.com")
+
+
+# Prompts response from API 
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
